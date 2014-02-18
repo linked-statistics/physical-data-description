af7b74081d10b54b26fa5666b5303e75c1de153d
boschthomas <thomas.bosch@gesis.org>
2014-02-18 19:05:00 +0100

    XMI2OWL.cmd

--- bindings/RDF/XMI2OWL/XMI2OWL.cmd
+++ bindings/RDF/XMI2OWL/XMI2OWL.cmd
@@ -1,3 +1,4 @@
-java -cp d:Progs\Saxon\saxon9.jar net.sf.saxon.Transform -s:XMI.xml -xsl:XMI2OWL.xsl -o:OWL.owl baseOntologyIRI=http://rdf-vocabulary.ddialliance.org/DDICoreVocabulary compositionName=contains
-	
+::java -cp C:\Daten\Software\Saxon\Saxon-B\9.1.0.8\saxon9.jar net.sf.saxon.Transform -s:XMI.xml -xsl:XMI2OWL.xsl -o:OWL.owl baseOntologyIRI=http://rdf-vocabulary.ddialliance.org/DDICoreVocabulary compositionName=contains
+java -cp C:\Daten\Software\Saxon\Saxon-B\9.1.0.8\saxon9.jar net.sf.saxon.Transform -s:phdd_xmi.xml -xsl:XMI2OWL.xsl -o:phdd.owl baseOntologyIRI=http://rdf-vocabulary.ddialliance.org/phdd compositionName=contains
+		
 PAUSE transformation successful!
